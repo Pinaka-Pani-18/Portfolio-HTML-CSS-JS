cdc788b9842ad0ded829520997f2289df96c1fd1
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-05-30 19:33:38 +0530

    experience & projects section are added

--- index.js
+++ index.js
@@ -30,3 +30,29 @@ var typed = new Typed('.text-type', {
     // backSpeed: 150,
     loop: true,
   });
+
+
+//   Projects filtering
+const btnEls =document.querySelectorAll("#projects .menu .btn");
+const projectBoxEls =document.querySelectorAll(".project-box");
+
+btnEls.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+
+document.querySelector(".activeProject").classList.remove("activeProject");
+        btn.classList.add("activeProject");
+
+        const name = e.target.dataset.name;
+        projectBoxEls.forEach((itemEl)=>{
+            if(name==="all"){
+                itemEl.style.display = "block"
+            }else{
+                if(itemEl.classList.contains(name)){
+                    itemEl.style.display = "block"
+                }else{
+                    itemEl.style.display = "none";
+                }
+            }
+        })
+    })
+})
